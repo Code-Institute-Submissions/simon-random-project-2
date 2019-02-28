f408e97aafda147fe008ca7b97296b76039c8073
Matthew Hopwood <boydancer@hotmail.co.uk>
2019-02-28 21:31:03 +0000

    added new function to button object to check for mouseclick and respond

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,50 @@
 var buttons = [];
 var imagePath = "assets/images/";
 
-function button(id, color)
-{
+function button(id, color) {
     var self = this;
     this.id = id;
     this.color = color;
+    this.state = false;
     this.object = document.getElementById(color + "_button");
-    this.flash = function()
-    {
+    this.object.addEventListener("click", function() {
+        self.pressed();
+    });
+    this.flash = function() {
         var self = this;
         this.object.src = imagePath + this.color + "_glow.png";
         this.timer = setTimeout(function() {
             self.object.src = imagePath + self.color + ".png";
         }, 500);
-    }
+    };
+    this.pressed = function() {
+        if (this.state)
+            return;
+
+        var self = this;
+        this.state = true;
+        this.object.src = imagePath + this.color + "_glow.png";
+        this.timer = setTimeout(function() {
+            self.object.src = imagePath + self.color + ".png";
+            self.state = false;
+        }, 500);
+    };
 }
 
-function flashButton()
-{
-    for (var t = 0; t < buttons.length; t++)
-    {
+function flashButton() {
+    for (var t = 0; t < buttons.length; t++) {
         buttons[t].flash();
     }
 }
 
-window.onload = function(e)
-{
-   buttons.push(new button(0, "yellow"));
-   buttons.push(new button(1, "blue"));
-   buttons.push(new button(2, "red"));
-   buttons.push(new button(3, "green"));
-   
-   flashButton();
-}
+$("#start_button").on("click", function() {
+    flashButton();
+    $("#start_button").html("Restart");
+});
+
+window.onload = function(e) {
+    buttons.push(new button(0, "yellow"));
+    buttons.push(new button(1, "blue"));
+    buttons.push(new button(2, "red"));
+    buttons.push(new button(3, "green"));
+};
