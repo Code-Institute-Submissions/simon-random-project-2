3cd6ae6182b2c25d576f86df47a3c3f296f3e02d
Matthew Hopwood <boydancer@hotmail.co.uk>
2019-02-28 20:51:05 +0000

    added working flash function for all buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,36 @@
 var buttons = [];
+var imagePath = "assets/images/";
 
 function button(id, color)
 {
-    self = this;
+    var self = this;
     this.id = id;
     this.color = color;
     this.object = document.getElementById(color + "_button");
+    this.flash = function()
+    {
+        var self = this;
+        this.object.src = imagePath + this.color + "_glow.png";
+        this.timer = setTimeout(function() {
+            self.object.src = imagePath + self.color + ".png";
+        }, 500);
+    }
+}
+
+function flashButton()
+{
+    for (var t = 0; t < buttons.length; t++)
+    {
+        buttons[t].flash();
+    }
 }
 
 window.onload = function(e)
 {
-   button.push(new button(0, "yellow"));
-   button.push(new button(1, "blue"));
-   button.push(new button(2, "red"));
-   button.push(new button(3, "green"));
+   buttons.push(new button(0, "yellow"));
+   buttons.push(new button(1, "blue"));
+   buttons.push(new button(2, "red"));
+   buttons.push(new button(3, "green"));
+   
+   flashButton();
 }
