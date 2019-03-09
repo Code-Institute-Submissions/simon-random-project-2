2c4f37f69b9465edf1cb8018ec4ebbb9627f8ca8
Matthew Hopwood <boydancer@hotmail.co.uk>
2019-03-09 17:54:13 +0000

    Added logic overview to js file, cleaned up code and started README

--- README.md
+++ README.md
@@ -1,26 +1,109 @@
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+// Game Overview
+
+    The game consists of four different coloured buttons as convention with Simon games; yellow, blue, red and green.
+    When the user presses the start button a random sequence of button flashes will occur (1 flash at level 1), which
+    the user will need to remember for their turn. As each buttons flashes it will also play a sound unique to that button.
+    
+    The start button is immediately replaced with a restart button, that allows the user to reset the game back to level 1.
+    Next to the start/restart button is a 'rules' button that once clicked will show a modal with game instructions (the amount
+    of text is reduced on mobile). 
+    
+    The user must remember the sequence that the AI plays, once it is the users turn they must 
+    input the same sequence (by clicking the corresponding colored buttons).Every time the user gets the sequence right, 
+    the level will increment by 1 and the sequence will become longer by 1. If the user get the sequence wrong, the level 
+    will automatically restart with a new random sequence but at the same level difficulty.
 
 
-Hi there! Welcome to Cloud9 IDE!
+// Logic Overview
 
-To get you started, we included a small hello world application.
-
-1) Open the hello-world.html file
-
-2) Click on the Preview button to open a live preview pane
-
-3) Make some changes to the file, save, watch the preview, and have fun!
-
-Happy coding!
-The Cloud9 IDE team
-
-
-## Support & Documentation
-
-Visit http://docs.c9.io for documentation, or http://support.c9.io for support.
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+    I created an array of 'button' objects that consist of the four colored button images the user can see on screen. I gave each button
+    a color and an ID number so they could be selected easily later on. The buttons have a flash and a pressed function. They also have a flag
+    to check whether or not they have been pressed, which is 'this.state = false'. I also use the 'self = this' method, which allows me to target
+    the instance of the button image within the button handler. The buttons are targeted by their ID values with 'document.getElementById'.
+    
+    The .pressed() function consists of a on click handler so that we can determine if the user clicks the button and allows us to carry out
+    the logic contained in the pressed function. This logic is basically change the source of the image to a new image that is slightly 
+    brighter to create the illusion of a 'flash'. A different sound is also played for each button, however we use the 'sound.currentTime = 0'
+    method to reset the sound playback so there aren't any overlapping sounds if the buttons are being pressed more quickly than the duration of
+    the sound file. In this function the 'state' property is set to true so that the button can only be pressed once, the 'state' property is then switched backed to
+    false after the 'setTimeout 400ms' function so the button can be pressed again.
+    
+    The .flash() function changes the button image source to a new image that is slightly brighter. There is a timer attached to the end of the function
+    that makes sure the image switches back to the original after 400ms, which creates the effect of a 'flash'. A sound is also played through this function.
+    
+    The generateSequence() function starts by emptying the sequences array so that each time it is called, the array contains only the newly generated numbers.
+    A for loop is used to generate a numbers between 0 and 3, depending on the current level (so level 3 would generate 3 numbers). The generated numbers
+    are then pushed into the 'sequences' array.
+    
+    The flashSequence() function contains the 'choice' variable which is equal to 'sequences[marker]', when first called is 'sequences[0]'. We then call the flash 
+    function which belongs to the 'button' object, but we choose the specific button from the array by matching the 'marker' with the index number in the 'buttons' array 
+    e.g.
+        
+        "
+        marker = 2
+        choice = sequences[marker]
+        choice = sequences[2]
+        sequences[2] = 0
+        buttons[choice].flash();
+        buttons[0].flash();
+        
+        "
+    this will select the 3rd item in the buttons array. This function will repeat while 'marker' is less than sequences.length (which is determined in the generateSquence
+    function). marker will always start at 0 and is incremented each time this function is called and will stop as it reachs the the length of sequences.
+    e.g.
+    
+        "
+        marker = 5
+        sequences.length = 6
+        
+        "
+    Instead of incrementing marker again, the else statement is ran which called endSequence().
+    
+    The endSequence() function resets the marker so that the next sequence will start from an index of 0 again. sequenceOver is also set to true,
+    which allows the user to press buttons.
+    
+    The roundOver(won) function uses an if / else statement to check if the user was successful in repeating the sequence. If not the level will 
+    restart itself with newly generated
+    sequence after 2000ms. If the user is successful then the level will be incremented by 1 before the new sequence is generated.
+    
+    The startGame() function calls the generateSequence function and then calls the flashSequence function after 1000ms.
+    
+    The restartGame() function is the same as startGame function, however it sets the level back to 1 and has a delay of 2000ms.
+    
+    jQuery is used to select the 'start_button' and attach an onclick handler. An if else statement is used to determine whether the 'start_button' 
+    will call startGame or restartGame.
+    
+    The window.onload = (e) function creates four button images that the player can interact with as soon as the window has loaded.
+    
+    I created a flag called 'allowInput' which is used thoughtout the code to limit the input of the user at certain parts of the game.
+    e.g.
+    
+        "
+         if (!allowInput)
+        {    
+            return;
+        }
+        "
+    
+    The check to see if the users sequence choice matches the AI sequence choice is present in the button.pressed() function. Using an if / else statement
+    I can check if the if the first number in the 'sequences' array is equal to the ID value given to the button that was pressed. It is possible to get the next number in the
+    'sequences' array by using a 'marker' and incrementing it everytime the use gets the button choice correct.
+    e.g.
+    
+    "
+    if (sequences[marker] !== this.id)
+    if (sequences[0] !== 1)
+    if (2 !== 1)
+    {
+        Execute loss code
+    }
+    
+    else (sequences[marker] == this.id)
+    else (sequences[0] == 3)
+    else (3 == 3)
+    {
+        Execute win code
+    }
+    
+    "
+    
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,115 @@
+/*
+    Below is the basic logical flow of the game:
+
+	Create a button object for each button image
+	Choose a number of random colours to create a sequence
+	Play the sequence to the player
+	Allow the player to select a sequence
+	If the player gets a sequence wrong then reset the level. If the player matches all sequences then end the round and go to the next level
+
+	More in-depth details of the logic:
+
+	The 'button' object is used to collate all data relating to the colored game buttons. It contains state data as well as functionality
+	for handling button presses and button flashes. Four 'button' objects are created and added to the 'buttons' array when the page has loaded.
+
+	At the start of a round the computer chooses a random sequence of colours via generateSequence(). The number of colors matches the players
+	current level). Once the sequence is generated flashSequence() is called to present the memory sequence to the player. User input is disabled
+	at this stage to prevent the user accidentally clicking on buttons.
+
+	In flashSequence() a timer is used to flash the buttons in the order of the chosen sequence. This gives the player the opportunity to see 
+	the sequence. During flashSequence(), the button object's flash() method is called to flash the button. Button flashing is achieved by
+	changing the src property of the buttons HTML Image to a bitmap of a brighter shade. After a short delay the button image's bitmap is set
+	back to the original bitmap to return the button to normal. Note that this same flash functionality is re-used when the player clicks on a
+	button during the next stage.
+
+	Once the computer finishes playing the memory sequence, input is enabled and the player is asked to replay the sequence by clicking on
+	buttons in the same order that the computer chose. Each time the player clicks a button the players choice is checked against the choices
+	that the computer made (within the 'sequences' array). The 'marker' variable is used to track which choice the player is currently taking so
+	the choice can be tested against the correct element of the 'sequences' array. Should the player select an incorrect choice then the game
+	is ended and the player is notified via roundOver(false). If the 'marker' reaches the total number of elements in the 'sequences' array then
+	this signifies that the player has selected all of the correct entries in the sequence and is notified that they have won the round via
+	roundOver(true).
+
+	At the end of a round, the player is shown the result of the round by writing the result to the message div in the DOM in roundOver().
+	If the user has won the round then their level is incremented and the next round started. If the user lost the round then the round is
+	repeated.
+*/
+
+// Array of button objects
 var buttons = [];
+
+// Array of sequences that the AI will take
 var sequences = [];
+
+// Starting game level
 var level = 1;
+
+// The marker for the index in the sequence
 var marker = 0;
+
+// A flag which marks the sequence playback over
 var sequenceOver = false;
+
+// A flag which marks the game as 'not in progress'
 var gameState = false;
+
+// A flag that allows the user input at certain stages of the game.
 var allowInput = true;
 
-
-function displayLevel() {
+// Reuseable function to display current level at different stages.
+function displayLevel() 
+{
     document.getElementById("level").innerHTML = "Level - '" + level + "'";
 }
 
-function displayMessage(text) {
+// Reusable function to display any message to the 'message' ID element.
+function displayMessage(text) 
+{
     document.getElementById("message").innerHTML = text;
 }
 
-function button(id, color) {
+// Game button object
+function button(id, color) 
+{
     var imagePath = "assets/images/";
-    var self = this;
+    var self = this;    // Store 'this' so it can be used inside the button handler
     
     this.id = id;
     this.color = color;
-    this.state = false;
+    this.state = false;    // A flag to say whether the instance of button has been pressed
     this.object = document.getElementById(color + "_button");
+    
+    // Add click event handler which calls pressed() function when the user clicks the image
     this.object.addEventListener("click", function() {
-        if (!allowInput)
+        if (!allowInput)    // Doesn't allow user input at this point if allowInput == false
+        {    
             return;
+        }
         self.pressed();
     });
-    this.flash = function() {
+    
+    // Flash function that when called will change the image to a new image
+    this.flash = function() 
+    {
         var self = this;
-        
-        this.object.src = imagePath + this.color + "_glow.png";
-        
         var sound = document.getElementById("sound" + this.id);
-        sound.play();
-        sound.currentTime = 0;
         
+        this.object.src = imagePath + this.color + "_glow.png";    // Change to new image
+        sound.play();    // Play sound
+        sound.currentTime = 0;    
+        
+        // Start a timer which will be called after 400ms that changes the new image back to the original
         this.timer = setTimeout(function() {
             self.object.src = imagePath + self.color + ".png";
         }, 400);
     };
-    this.pressed = function() {
-        if (this.state || !sequenceOver || marker >= sequences.length) {
+    
+    // Pressed function is called when the user clicks on a game button. This function will flash the image
+    // and then perform a check to see if the user clicks the correct color in the sequence.
+    this.pressed = function() 
+    {
+        if (this.state || !sequenceOver || marker >= sequences.length) 
+        {
             return;
         }
         var self = this;
@@ -58,76 +125,96 @@ function button(id, color) {
             self.object.src = imagePath + self.color + ".png";
             self.state = false;
         }, 400);
-        if (sequences[marker] !== this.id) {
-            roundOver(false);
-        } else {
+        if (sequences[marker] !== this.id)  // Check to see is the number chosen in the sequence is NOT the same as the button ID.
+        {
+            roundOver(false);   // Users choice did not match the sequence so round is lost
+        } 
+        else 
+        {
             marker++;
-            if (marker >= sequences.length) {
-                roundOver(true);
+            if (marker >= sequences.length)    // Checks to see if all the numbers in the sequence have been matched
+            {
+                roundOver(true);    // Users manges to match all choices in the sequence so round is won
             }
         }
     };
 }
 
 function generateSequence() {
-    sequences = [];
+    sequences = [];    // Empties the array each time the function is called to start a new sequence
+    
+    // Generates a random number sequence. The total number is equal to the level number. 
     for (var t = 0; t < level; t++)
     {
-        sequences.push(Math.random() * 4 | 0);
+        sequences.push(Math.random() * 4 | 0);  // Picks a number between 0 and 3 and pushes to sequences array
     }
 }
 
 function flashSequence() {
-   allowInput = false;
+   allowInput = false;   // No input allowed until the function has ended
    var choice = sequences[marker];
-   buttons[choice].flash();
-   marker++;
+   buttons[choice].flash();    // Flashes the button that has the same index as the the sequence choice
+   marker++;    
    if (marker < sequences.length)
    {
-      setTimeout(function() {
-          flashSequence();
-      }, 1000);
-   } else {
-      endSequence();
+        // Start a timer to flash the sequence
+        setTimeout(function() {
+            flashSequence();
+        }, 1000);
+   } 
+   else 
+   {    
+        // We got to the end of the sequence so we end sequence instead of restarting timer
+        endSequence();
    }
 }
 
-function endSequence() {
+function endSequence() 
+{
+    // Reset the marker and mark the sequence as over so the user can press the buttons
     marker = 0;
     allowInput = true;
     sequenceOver = true;
     displayMessage("Press the buttons in the same order, good luck!");
 }
 
-function roundOver(won) {
+function roundOver(won) 
+{
     sequenceOver = false;
-    if (!won) {
+    if (!won) 
+    {
         displayMessage("Round over friend, try again!");
-    } else {
+    } 
+    else 
+    {
         displayMessage("Round won friend, continue to the next round!");
-        level++;
+        level++;    // Increment level if round is won
     }
     
+    // Move to next round after 2s
     setTimeout(function() {
         startGame();
     }, 2000);
     
 }
 
-function startGame() {
+function startGame() 
+{
     marker = 0;
     displayLevel();
     displayMessage("Remember the sequence!")
-    generateSequence();
+    generateSequence();    // Generates new sequence with new numbers with a lenth equal to level number
+    // After 1s the starting sequence will begin to flash
     setTimeout(function() {
         flashSequence();    
     }, 1000);
     
 }
 
-function restartGame() {
+function restartGame() 
+{
     marker = 0;
-    level = 1;
+    level = 1;  // Sets the level back to 1 
     displayLevel(); 
     displayMessage("Remember the sequence!")
     generateSequence();
@@ -137,23 +224,27 @@ function restartGame() {
 }
 
 $("#start_button").on("click", function() {
-    if (!allowInput)
+    if (!allowInput) // The user cannot click the button unless allowInput == true
     {
         return;
     }
-    allowInput = false;
-    if (gameState === false)
+    allowInput = false; // This flag makes sure the user can only click the button until the sequence is over 
+    if (gameState === false) 
     {
         startGame();
         $("#start_button").html("Restart");
-        gameState = true;
-    } else {
-        restartGame();
+        gameState = true;    // Once game has started it will always be 'in progress' so this code is only be ran once.
+    } 
+    else 
+    {
+        restartGame(); 
     }
     $(".img_buttons").addClass('cursor_pointer');
 });
 
-window.onload = function(e) {
+window.onload = function(e) 
+{
+    // Create the four image buttons that the player can interact with
     buttons.push(new button(0, "yellow"));
     buttons.push(new button(1, "blue"));
     buttons.push(new button(2, "red"));
